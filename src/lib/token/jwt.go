0dd57693ca5fcb0f0c68fd28cd74564f50b97afc
nickgarfield <ntgarfield@gmail.com>
2020-09-15 11:12:07 -0500

    📖 DOC: Token generation and validation

--- src/lib/token/jwt.go
+++ src/lib/token/jwt.go
@@ -18,12 +18,12 @@ func JWT(
 	ctx context.Context,
 	claims map[string]interface{},
 	expiresAt time.Time,
-	sig jwt.SigningMethod,
+	signingMethod jwt.SigningMethod,
 	secrets secrets.Manager,
 ) (string, error) {
 
 	// Create new token
-	tkn := jwt.New(sig)
+	tkn := jwt.New(signingMethod)
 	for k, v := range claims {
 		tkn.Claims.(jwt.MapClaims)[k] = v
 	}
@@ -31,7 +31,10 @@ func JWT(
 
 	// TODO add issuer
 
-	switch sig {
+	// Depending on the selected signing method...
+	switch signingMethod {
+
+	// For HS256, fetch the shared secret
 	case jwt.SigningMethodHS256:
 		sec, err := secrets.GetString("jws.hs256.secret")
 		if err != nil {
@@ -39,6 +42,7 @@ func JWT(
 		}
 		return tkn.SignedString([]byte(sec))
 
+	// For RS256, fetch one of the RSA256 private signing key
 	case jwt.SigningMethodRS256:
 		secs, err := secrets.GetMap("jws.rs256")
 		if err != nil {
@@ -48,13 +52,13 @@ func JWT(
 		// Naive strategy: choose first RSA256 key
 		// TODO pick an available key at random
 		for sec := range secs {
-			if kid, ok := sec.(string); ok {
-				tkn.Claims.(jwt.MapClaims)["kid"] = kid
-				strBase64, err := secrets.GetString(fmt.Sprintf("jws.rs256.%v.private", kid))
+			if keyID, ok := sec.(string); ok {
+				tkn.Header["kid"] = keyID
+				keyStr, err := secrets.GetString(fmt.Sprintf("jws.rs256.%v.private", keyID))
 				if err != nil {
 					return "", err
 				}
-				key, err := parseRSA256PrivateKey(strBase64)
+				key, err := parseRSA256PrivateKey(keyStr)
 				if err != nil {
 					return "", err
 				}
@@ -63,35 +67,44 @@ func JWT(
 		}
 		return "", fmt.Errorf("No RSA256 signing key found")
 
+	// Return error if selected signing method is unsupported
 	default:
-		return "", fmt.Errorf("Signing method %v not supported", sig)
+		return "", fmt.Errorf("Signing method '%v' not supported", signingMethod)
 	}
 }
 
+// parseRSA256PublicKey parses a RS256 private key from a base64 encoded string
 func parseRSA256PrivateKey(str string) (*rsa.PrivateKey, error) {
+
+	// Base64 decode string
 	keyBytes, err := base64.StdEncoding.DecodeString(str)
 	if err != nil {
 		return nil, err
 	}
 
+	// Decode PEM
 	keyPem, _ := pem.Decode(keyBytes)
 	if keyPem == nil {
 		return nil, fmt.Errorf("Failed to get secret")
 	}
+
+	// Validate PEM type
 	if keyPem.Type != "RSA PRIVATE KEY" {
 		return nil, fmt.Errorf("Decoded key is of the wrong type (%v)", keyPem.Type)
 	}
 
+	// Parse to key bytes
 	var parsedKey interface{}
 	if parsedKey, err = x509.ParsePKCS1PrivateKey(keyPem.Bytes); err != nil {
 		if parsedKey, err = x509.ParsePKCS8PrivateKey(keyPem.Bytes); err != nil {
 			return nil, err
 		}
 	}
+
+	// Cast to RSA private key type
 	privKey, ok := parsedKey.(*rsa.PrivateKey)
 	if !ok {
 		return nil, fmt.Errorf("Failed to parse private key")
 	}
-
 	return privKey, nil
 }
--- src/lib/token/validate.go
+++ src/lib/token/validate.go
@@ -16,32 +16,40 @@ import (
 func Validate(
 	ctx context.Context,
 	jwtStr string,
-	sig jwt.SigningMethod,
+	signingMethod jwt.SigningMethod,
 	secrets secrets.Manager,
 ) (map[string]interface{}, error) {
 
 	// Parse the JWT string
-	tkn, err := jwt.Parse(jwtStr, getValidationKey(sig, secrets))
+	tkn, err := jwt.Parse(jwtStr, getValidationKey(signingMethod, secrets))
 	if err != nil {
 		return nil, err
 	}
 
 	// TODO validate issuer
 
+	// Return extracted claims
 	if claims, ok := tkn.Claims.(jwt.MapClaims); ok && tkn.Valid {
 		return claims, nil
 	}
-
 	return nil, fmt.Errorf("Token is invalid")
 }
 
-func getValidationKey(sig jwt.SigningMethod, secrets secrets.Manager) jwt.Keyfunc {
+// getValidationKey inspects a token and the expected signing method and returns a key for validating the signature
+func getValidationKey(
+	signingMethod jwt.SigningMethod,
+	secrets secrets.Manager,
+) jwt.Keyfunc {
+
 	return func(t *jwt.Token) (interface{}, error) {
-		if t.Method != sig {
+		if t.Method != signingMethod {
 			return nil, fmt.Errorf("Unexpected signing method: %v", t.Header["alg"])
 		}
 
-		switch sig {
+		// Depending on the expected signing method...
+		switch signingMethod {
+
+		// For HS256, fetch the shared secret
 		case jwt.SigningMethodHS256:
 			sec, err := secrets.GetString("jws.hs256.secret")
 			if err != nil {
@@ -49,59 +57,61 @@ func getValidationKey(sig jwt.SigningMethod, secrets secrets.Manager) jwt.Keyfun
 			}
 			return []byte(sec), nil
 
+		// For RSA256, fetch the public key for the token's KID
 		case jwt.SigningMethodRS256:
-			secs, err := secrets.GetMap("jws.rs256")
+			keyID, ok := t.Header["kid"].(string)
+			if !ok {
+				return nil, fmt.Errorf("Header 'kid' not found")
+			}
+			keyStr, err := secrets.GetString(fmt.Sprintf("jws.rs256.%v.public", keyID))
 			if err != nil {
 				return nil, err
 			}
-
-			// Naive strategy: choose first RSA256 key
-			// TODO pick an available key at random
-			for sec := range secs {
-				if kid, ok := sec.(string); ok {
-					strBase64, err := secrets.GetString(fmt.Sprintf("jws.rs256.%v.public", kid))
-					if err != nil {
-						return nil, err
-					}
-					key, err := parseRSA256PublicKey(strBase64)
-					if err != nil {
-						return nil, err
-					}
-					return key, err
-				}
+			key, err := parseRSA256PublicKey(keyStr)
+			if err != nil {
+				return nil, err
 			}
-			return nil, fmt.Errorf("No RSA256 public key found")
+			return key, err
 
+		// Return error if expected signing method is unsupported
 		default:
-			return nil, fmt.Errorf("Validation method '%v' not supported", sig)
+			return nil, fmt.Errorf("Validation method '%v' not supported", signingMethod)
 		}
 	}
 }
 
+// parseRSA256PublicKey parses a RS256 public key from a base64 encoded string
 func parseRSA256PublicKey(str string) (*rsa.PublicKey, error) {
+
+	// Base64 decode string
 	keyBytes, err := base64.StdEncoding.DecodeString(str)
 	if err != nil {
 		return nil, err
 	}
 
+	// Decode PEM
 	keyPem, _ := pem.Decode(keyBytes)
 	if keyPem == nil {
 		return nil, fmt.Errorf("Failed to get secret")
 	}
+
+	// Validate PEM type
 	if keyPem.Type != "RSA PUBLIC KEY" {
 		return nil, fmt.Errorf("Decoded key is of the wrong type (%v)", keyPem.Type)
 	}
 
+	// Parse to key bytes
 	var parsedKey interface{}
 	if parsedKey, err = x509.ParsePKIXPublicKey(keyPem.Bytes); err != nil {
 		if parsedKey, err = x509.ParsePKCS1PublicKey(keyPem.Bytes); err != nil {
 			return nil, err
 		}
 	}
+
+	// Cast to RSA public key type
 	pubKey, ok := parsedKey.(*rsa.PublicKey)
 	if !ok {
 		return nil, fmt.Errorf("Failed to parse public key")
 	}
-
 	return pubKey, nil
 }
